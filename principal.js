1aff32ebd7fd80cae3deba43e7d48d1450692d2c
alerubira <100098255+alerubira@users.noreply.github.com>
2024-04-03 21:22:12 -0300

    iniciando

---
+++ principal.js
@@ -0,0 +1,14 @@
+fetch('https://fakestoreapi.com/products')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parsea la respuesta JSON
+  })
+  .then(data => {
+    // Hacer algo con los datos obtenidos
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
